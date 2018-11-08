a35d61e93ff411d80cf8bb0c85765d07f7a9887c
mac <1>
2018-11-08 10:52:28 +0800

    1.1.0发布

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-/** @format */
 /**
  * @author youxuan  E-mail:xuanyouwu@163.com
  * @Description 网络客户端
  */
-import {XFClient} from "./xxf_http/XFClient";
-import {XFClientBuilder} from "./xxf_http/XFClientBuilder";
-import {Exception} from './xxf_base/exceptions/Exception';
-import {HttpException} from './xxf_base/exceptions/HttpException';
-
-export {
-    XFClient,
-    XFClientBuilder,
-}
-
 
 import {AppRegistry} from 'react-native';
 import App from './App';
